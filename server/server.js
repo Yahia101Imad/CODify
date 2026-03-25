00dbd07dfba8c5727c523080ac620cd25381ec42
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-03-25 18:21:22 +0100

    feat: add the server setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
